8717dd815c64065f6d4ae92667d654396ec04d70
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-05-29 17:23:20 +0100

    added spend history route

--- src/main/java/com/makers/moneytree/controller/SpendUpload/SpendUploadController.java
+++ src/main/java/com/makers/moneytree/controller/SpendUpload/SpendUploadController.java
@@ -1,5 +1,7 @@
 package com.makers.moneytree.controller.SpendUpload;
 
+import com.makers.moneytree.repository.SpendAnalysis.SpendAnalysisRepository;
+import com.makers.moneytree.services.SpendAnalysis.SpendAnalysisService;
 import com.makers.moneytree.services.SpendUpload.SpendUploadService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +16,24 @@ public class SpendUploadController {
 
     private final SpendUploadService spendUploadService;
 
+    private SpendAnalysisService spendAnalysisService;
+
     public SpendUploadController(SpendUploadService spendUploadService) {
         this.spendUploadService = spendUploadService;
+    }
+
+    public SpendAnalysisController(SpendAnalysisService spendAnalysisService) {
+        this.spendAnalysisService = spendAnalysisService;
     }
 
     @GetMapping("/spendtotals")
     public ResponseEntity<List> getSpend() {
         return ResponseEntity.ok(List.of(123, 321, 123, 789, 537));
+    };
+
+    @GetMapping("/spendhistory")
+    public ResponseEntity<List> getSpendHistory() {
+        return ResponseEntity.ok(spendAnalysisService.findSpendHistory());
     };
 
     @PostMapping(value = "/spendtotals/add", consumes = {"multipart/form-data"} )
