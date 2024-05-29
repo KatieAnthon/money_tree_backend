d40763afdfb1dec19dd195f2c1533801555bd3fb
KatieAnthon <katieanthonisz@hotmail.co.uk>
2024-05-29 17:54:59 +0100

    spend controllers merged

---
+++ src/main/java/com/makers/moneytree/controller/SpendUpload/SpendAnalysisController.java
@@ -0,0 +1,37 @@
+package com.makers.moneytree.controller.SpendUpload;
+
+import com.makers.moneytree.services.SpendAnalysis.SpendAnalysisService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/spendanalysis")
+public class SpendAnalysisController {
+
+    private SpendAnalysisService spendAnalysisService;
+
+    public SpendAnalysisController(SpendAnalysisService spendAnalysisService) {
+        this.spendAnalysisService = spendAnalysisService;
+    }
+    @GetMapping("/spendtotals")
+    public ResponseEntity<List> getSpend() {
+        return ResponseEntity.ok(List.of(123, 321, 123, 789, 537));
+    };
+
+    @GetMapping("/spendhistory")
+    public ResponseEntity<List> getSpendHistory() {
+        return ResponseEntity.ok(spendAnalysisService.findSpendHistory());
+    };
+    @PostMapping(value = "/spendtotals/add", consumes = {"multipart/form-data"} )
+    public ResponseEntity<Integer> uploadStatement(
+            @RequestParam("file")MultipartFile file) throws IOException {
+        return ResponseEntity.ok(spendAnalysisService.addSpendFile(file));
+
+    }
+}
+
+
--- src/main/java/com/makers/moneytree/services/SpendAnalysis/SpendAnalysisService.java
+++ src/main/java/com/makers/moneytree/services/SpendAnalysis/SpendAnalysisService.java
@@ -2,21 +2,74 @@ package com.makers.moneytree.services.SpendAnalysis;
 
 import com.makers.moneytree.model.SpendAnalysis.SpendAnalysis;
 import com.makers.moneytree.repository.SpendAnalysis.SpendAnalysisRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Service
 public class SpendAnalysisService {
 
-    private final SpendAnalysisRepository spendAnalysisRepository;
-    @Autowired
-    public SpendAnalysisService(SpendAnalysisRepository spendAnalysisRepository)
-    {
-        this.spendAnalysisRepository = spendAnalysisRepository;
-    };
+    private final SpendAnalysisRepository repository;
+
+    public SpendAnalysisService(SpendAnalysisRepository repository) {
+        this.repository = repository;
+    }
 
     public List<SpendAnalysis> findSpendHistory() {
-        return spendAnalysisRepository.findAll();
+        return repository.findAll();
+    }
+
+    public Integer addSpendFile(MultipartFile file) throws IOException {
+        Set<SpendAnalysis> spendAnalysis = parseCSV(file);
+        repository.saveAll(spendAnalysis);
+        return spendAnalysis.size();
+    }
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private Set<SpendAnalysis> parseCSV(MultipartFile file) throws IOException {
+        try(Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            HeaderColumnNameMappingStrategy<SpendCsvRepresentation> strategy =
+                    new HeaderColumnNameMappingStrategy<>();
+            // Set the parsing strategy
+            strategy.setType(SpendCsvRepresentation.class);
+            CsvToBean<SpendCsvRepresentation> csvToBean =
+                    new CsvToBeanBuilder<SpendCsvRepresentation>(reader)
+                            .withMappingStrategy(strategy)
+                            .withIgnoreEmptyLine(true)
+                            .withIgnoreLeadingWhiteSpace(true)
+                            .build();
+
+            return csvToBean.parse()
+                    .stream()
+                    .map(csvLine -> {
+                            LocalDate date = LocalDate.parse(csvLine.getDate(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                            Double amount = Double.parseDouble(csvLine.getAmount());
+
+                            return new SpendAnalysis(
+                            null,
+                            null,
+                            date,
+                            csvLine.getName(),
+                            amount,
+                            csvLine.getCurrency(),
+                            null);
+                    })
+                    .collect(Collectors.toSet());
+
+        }
+
     }
 
 }
